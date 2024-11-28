a9549274664ba9d5accd65152f960612499cec2d
hectorsantiagoesquivel <vagrant@testing>
2024-11-28 16:52:46 +0000

    cambios

--- Bash_script/script_discos.sh
+++ Bash_script/script_discos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Creando LVM 5G" 
+echo "Creando LVM vg_datos" 
 sudo fdisk /dev/sdc <<EOF
 n
 
@@ -22,28 +22,28 @@ sudo vgcreate vg_datos /dev/sdc1
 sudo lvcreate -L +10M vg_datos -n lv_docker
 sudo lvcreate -L +2.5G vg_datos -n lv_workareas
 
-sudo mkfs.ext4 /dev/mapper/vg_datos-lv_docker
-sudo mkfs.ext4 /dev/mapper/vg_datos-lv_workareas
+sudo mkfs.ext4 /dev/vg_datos/lv_docker
+sudo mkfs.ext4 /dev/vg_datos/lv_workareas
 
 sudo mkdir -p /var/lib/docker
 sudo mkdir -p /work
 
-echo "Montando"
+echo "Montando lv docker y lv workareas"
 
-echo "/dev/mapper/vg_datos-lv_docker /var/lib/docker ext4 defaults 0 0" | sudo tee -a /etc/fstab
-echo "/dev/mapper/vg_datos-lv_workareas /work ext4 defaults 0 0" | sudo tee -a /etc/fstab
-
-
+echo "/dev/vg_datos/lv_docker /var/lib/docker ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/vg_datos/lv_workareas /work ext4 defaults 0 0" | sudo tee -a /etc/fstab
 
 
-echo "Creando lvm 3G"
+
+
+echo "Creando vg temp"
 sudo fdisk /dev/sdd << EOF
 n
 
 
 
 
-t
+ti
 8e
 w
 EOF
@@ -56,15 +56,15 @@ sudo vgcreate vg_temp /dev/sdd1
 
 sudo lvcreate -L +2.5G vg_temp -n lv_swap
 
-sudo mkswap /dev/mapper/vg_temp-lv_swap
+sudo mkswap /dev/vg_temp/lv_swap
 
 
-echo "Montando"
+echo "Montando lv swap"
 
-echo "/dev/mapper/vg_temp-lv_swap none swap sw 0 0" | sudo tee -a /etc/fstab
+echo "/dev/vg_temp/lv_swap none swap sw 0 0" | sudo tee -a /etc/fstab
 
 
-echo "Creando lvm 2G"
+echo "Creando swap"
 sudo fdisk /dev/sde << EOF
 n
 
@@ -80,7 +80,7 @@ sudo wipefs -a /dev/sde1
 
 sudo mkswap /dev/sde1
 
-echo "Montando"
+echo "Montando swap"
 
 echo "/dev/sde1 none swap sw 0 0" | sudo tee -a /etc/fstab
 
